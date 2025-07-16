9c147a4056b5ae4ddc3c62be2ae97c25b4aeb97d
rigueti-bruno <bruno@rigueti.com>
2025-07-16 16:52:49 -0300

    removido contato e sobre

--- curso-django-projeto-1/recipes/urls.py
+++ curso-django-projeto-1/recipes/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from recipes.views import home, sobre, contato
+from recipes.views import home
 
 urlpatterns = [
     path('',home), # Inclui o conteudo da view home na pagina inicial/raiz do site
-    path('sobre/',sobre), # inclui o conteudo da view sobre na pagina sobre
-    path('contato/',contato) # inclui o conteudo da view contato na pagina contato
-]
+    ]
--- curso-django-projeto-1/recipes/views.py
+++ curso-django-projeto-1/recipes/views.py
@@ -5,9 +5,3 @@ from django.http import HttpResponse # dependencia da view - cria a response
 
 def home(request): # view home
     return render(request,'recipes/home.html',status=201, context={'name':"Bruno"})
-
-def sobre(request):
-    return HttpResponse('SOBRE 3') # função que retorna uma "Response" genérica indicada pela função HTTPResponse
-
-def contato(request):
-    return HttpResponse('CONTATO 3')
