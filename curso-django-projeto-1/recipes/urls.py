987c37c935648c38505ecfa75e2d6f4e795a2637
rigueti-bruno <bruno@rigueti.com>
2025-09-13 22:16:27 -0300

    Aprofundado na criação de partials e na abstração de partes do HTML

--- curso-django-projeto-1/recipes/urls.py
+++ curso-django-projeto-1/recipes/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from recipes.views import home
+from recipes.views import home # importação das views do app recipes
 
 urlpatterns = [
     path('',home), # Inclui o conteudo da view home na pagina inicial/raiz do site
--- curso-django-projeto-1/recipes/views.py
+++ curso-django-projeto-1/recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render # randeriza os arquivos html e css do site
 
 # Create your views here.
 from django.http import HttpResponse # dependencia da view - cria a response
