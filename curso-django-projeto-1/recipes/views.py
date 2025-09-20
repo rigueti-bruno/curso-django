7f6b4360b8f67705c6de736a16c45996e3f80438
rigueti-bruno <bruno@rigueti.com>
2025-09-20 20:20:19 -0300

    Conclusão da página do exemplo e Introdução aos Static Files

--- curso-django-projeto-1/recipes/views.py
+++ curso-django-projeto-1/recipes/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render # randeriza os arquivos html e css do site
 from django.http import HttpResponse # dependencia da view - cria a response
 
 def home(request): # view home
-    return render(request,'pages/home.html',status=201, context={'name':"Bruno"})
+    return render(request,'recipes/pages/home.html',status=201, context={'name':"Bruno"})
