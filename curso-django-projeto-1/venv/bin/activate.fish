33056b25fb5958afb6d6e1fe508b31b1700b2aff
rigueti-bruno <bruno@rigueti.com>
2025-09-20 08:15:50 -0300

    Ajuste no venv

--- curso-django-projeto-1/venv/bin/activate.fish
+++ curso-django-projeto-1/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/bruno_rigueti/curso-django/curso-django-projeto-1/venv
+set -gx VIRTUAL_ENV /home/bruno/curso-django/curso-django-projeto-1/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
