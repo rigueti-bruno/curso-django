e1a0f0df73444d14789f573724780b6897b661d6
rigueti-bruno <bruno@rigueti.com>
2025-10-01 23:45:21 -0300

    Injetando dados gerados no Template

--- curso-django-projeto-1/recipes/views.py
+++ curso-django-projeto-1/recipes/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render # randeriza os arquivos html e css do site
 # Create your views here.
 from django.http import HttpResponse # dependencia da view - cria a response
 
+# importa a factory
+from utils.recipes.factory import make_recipe
+
 def home(request): # view home
-    return render(request,'recipes/pages/home.html', context={'name':"Bruno"})
+    return render(request,'recipes/pages/home.html', context={'recipes':[make_recipe() for _ in range(10)]})
+# a tag 'recipes' receberá os dados gerados pela função make_recipe e com ela serão inseridos os dados no template
 
 def recipe(request, id): # view recipe
-    return render(request,'recipes/pages/recipe-view.html', context={'name':"Bruno"})
+    return render(request,'recipes/pages/recipe-view.html', context={'recipe':make_recipe()})
