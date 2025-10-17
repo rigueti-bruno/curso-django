ea6be0d8d6ba160732137547b3c5ce3e7671725e
rigueti-bruno <bruno@rigueti.com>
2025-10-16 23:11:44 -0300

    Começando a injetar dados da base de dados no arquivo html

--- curso-django-projeto-1/recipes/urls.py
+++ curso-django-projeto-1/recipes/urls.py
@@ -6,5 +6,6 @@ app_name = 'recipes' # namespace do app recipes
 
 urlpatterns = [
     path('', views.home,name='home'), # Inclui o conteudo da view home na pagina inicial/raiz do site
+    path('recipes/category/int:category_id/',views.category,name='category'), # Inclui o conteudo da view category na pagina recipes/category/int:category_id/
     path('recipe/<int:id>/', views.recipe,name='recipe'), # Inclui o conteudo da view recipe na pagina recipe/i
     ]
--- curso-django-projeto-1/recipes/views.py
+++ curso-django-projeto-1/recipes/views.py
@@ -6,9 +6,21 @@ from django.http import HttpResponse # dependencia da view - cria a response
 # importa a factory
 from utils.recipes.factory import make_recipe
 
+# importa o model Recipe
+from .models import Recipe
+
 def home(request): # view home
-    return render(request,'recipes/pages/home.html', context={'recipes':[make_recipe() for _ in range(10)]})
-# a tag 'recipes' receberá os dados gerados pela função make_recipe e com ela serão inseridos os dados no template
+    recipes = Recipe.objects.all().order_by('-id') # obtem os dados da tabela Recipe e ordena por id decrescente
+    return render(request,'recipes/pages/home.html', context={
+        'recipes':recipes #substituimos os dados gerados aleatoriamente pela consulta ao banco de dados
+        })
+    
+def category(request,category_id): # view category
+    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id') # obtem os dados da tabela Recipe e ordena por id decrescente
+    return render(request,'recipes/pages/home.html', context={
+        'recipes':recipes #substituimos os dados gerados aleatoriamente pela consulta ao banco de dados
+        })
+
 
 def recipe(request, id): # view recipe
     return render(request,'recipes/pages/recipe-view.html', context={
