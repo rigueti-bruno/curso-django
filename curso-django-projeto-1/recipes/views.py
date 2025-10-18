ab11882af5ae3d8aa81525069ef607dba9ca990b
rigueti-bruno <bruno@rigueti.com>
2025-10-18 17:52:18 -0300

    Lidando com páginas não encontradas - http 404

--- curso-django-projeto-1/recipes/views.py
+++ curso-django-projeto-1/recipes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render # randeriza os arquivos html e css do site
 
+from django.shortcuts import get_list_or_404 # importa o atalho para retornar 404 se a lista estiver vazia
+
 # Create your views here.
 from django.http import HttpResponse # dependencia da view - cria a response
 
@@ -8,6 +10,9 @@ from utils.recipes.factory import make_recipe
 
 # importa o model Recipe
 from .models import Recipe
+
+# Importa as respostas de erro HTTP
+from django.http import HttpResponse, Http404
 
 def home(request): # view home
     recipes = Recipe.objects.filter(
@@ -24,10 +29,12 @@ def recipe(request, id): # view recipe
 })
 
 def category(request,category_id): # view category
-    recipes = Recipe.objects.filter(
+    recipes = get_list_or_404(Recipe.objects.filter( # o get_list_or_404 retorna 404 se a lista estiver vazia
         category__id=category_id,
         is_published=True,
-        ).order_by('-id') # obtem os dados da tabela Recipe e ordena por id decrescente
+        ).order_by('-id')) # obtem os dados da tabela Recipe e ordena por id decrescente
+    
     return render(request,'recipes/pages/category.html', context={
-        'recipes': recipes #substituimos os dados gerados aleatoriamente pela consulta ao banco de dados
+        'recipes': recipes, #substituimos os dados gerados aleatoriamente pela consulta ao banco de dados
+        'title': f'{recipes[0].category.name} - Category', #pega o nome da categoria do primeiro item da lista
 })
