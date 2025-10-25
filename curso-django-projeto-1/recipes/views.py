d80aba2d2fedc4d1bcdf092887b5c0013777a8f9
rigueti-bruno <bruno@rigueti.com>
2025-10-25 18:37:57 -0300

    Ajustes finais nos views e templates

--- curso-django-projeto-1/recipes/views.py
+++ curso-django-projeto-1/recipes/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render # randeriza os arquivos html e css do site
 
 from django.shortcuts import get_list_or_404 # importa o atalho para retornar 404 se a lista estiver vazia
 
+from django.shortcuts import get_object_or_404 # importa o atalho para retornar 404 se o objeto nao for encontrado
+
 # Create your views here.
 from django.http import HttpResponse # dependencia da view - cria a response
 
@@ -15,17 +17,17 @@ from .models import Recipe
 from django.http import HttpResponse, Http404
 
 def home(request): # view home
-    recipes = Recipe.objects.filter(
-        is_published=True
-        ).order_by('-id') # obtem os dados da tabela Recipe e ordena por id decrescente
+    recipes = get_list_or_404(Recipe,is_published=True)# obtem os dados da tabela Recipe e ordena por id decrescente
     return render(request,'recipes/pages/home.html', context={
         'recipes': recipes #substituimos os dados gerados aleatoriamente pela consulta ao banco de dados
         })
     
 def recipe(request, id): # view recipe
-    recipes = Recipe.objects.all().order_by('-id') # obtem os dados da tabela Recipe e ordena por id decrescente
+    recipe = get_object_or_404(Recipe, pk=id,is_published=True) # obtem os dados da tabela Recipe pelo id ou retorna 404 se nao encontrar
+    
     return render(request,'recipes/pages/recipe-view.html', context={
-        'recipes': recipes #substituimos os dados gerados aleatoriamente pela consulta ao banco de dados
+        'recipe': recipe, #substituimos os dados gerados aleatoriamente pela consulta ao banco de dados
+        'is_detail_page': True,
 })
 
 def category(request,category_id): # view category
