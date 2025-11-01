47c10ff6a3997057e2c60c5da0cee4144cbe27d0
rigueti-bruno <bruno@rigueti.com>
2025-11-01 19:38:23 -0300

    Métodos para otimização de consultas a bases de dados pelo Django

--- curso-django-projeto-1/recipes/urls.py
+++ curso-django-projeto-1/recipes/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('', views.home,name='home'), # Inclui o conteudo da view home na pagina inicial/raiz do site
     path('recipes/category/<int:category_id>/',views.category,name='category'), # Inclui o conteudo da view category na pagina recipes/category/int:category_id/
     path('recipes/<int:id>/', views.recipe,name='recipe'), # Inclui o conteudo da view recipe na pagina recipe/i
+    path(
+        'recipes/theory/',
+         views.theory,
+         name='theory'), # Inclui o conteudo da view theory na pagina recipe/theory/
     ]
--- curso-django-projeto-1/recipes/views.py
+++ curso-django-projeto-1/recipes/views.py
@@ -16,6 +16,13 @@ from .models import Recipe
 # Importa as respostas de erro HTTP
 from django.http import HttpResponse, Http404
 
+# Importa o erro de valor inexistente:
+from django.core.exceptions import ObjectDoesNotExist
+
+from django.db.models import Q
+
+from django.db.models import F
+
 def home(request): # view home
     recipes = get_list_or_404(Recipe,is_published=True)# obtem os dados da tabela Recipe e ordena por id decrescente
     return render(request,'recipes/pages/home.html', context={
@@ -40,3 +47,16 @@ def category(request,category_id): # view category
         'recipes': recipes, #substituimos os dados gerados aleatoriamente pela consulta ao banco de dados
         'title': f'{recipes[0].category.name} - Category', #pega o nome da categoria do primeiro item da lista
 })
+
+def theory(request,*args,**kwargs):
+    recipes = Recipe.objects.values('id','title','description','author__username').filter(
+        Q(id__gt = 1) |
+        Q(author__username__icontains='maria')
+        ).order_by('-id')[:10]
+    context = {'recipes':recipes} # contexto da view, 'recipes':recipes recebe os dados do queryset
+    return render(
+        request,
+        'recipes/pages/theory.html',
+        context=context
+    )
+    
