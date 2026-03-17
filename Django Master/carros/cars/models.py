ef9c75ef499029520b45b0e6d1e26a9b3040067e
rigueti-bruno <bruno@rigueti.com>
2026-03-16 22:19:33 -0300

    Criação do primeiro Model do projeto

--- Django Master/carros/cars/models.py
+++ Django Master/carros/cars/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Tabela que receberá os registros dos carros:
+class Car(models.Model):
+    id = models.AutoField(primary_key=True) # identificador único do registro de cada carro
+    model = models.CharField(max_length=200) # modelo do carro
+    brand = models.CharField(max_length=200) # marca do carro
+    factory_year = models.IntegerField(blank=True, null=True) # ano de fabricação do carro
+    model_year = models.IntegerField(blank=True, null=True) # ano do modelo do carro
+    value = models.FloatField(blank=True, null=True) # valor do carro
