32a112f21b845537cd9b0d757652a32b6e138ce0
rigueti-bruno <bruno@rigueti.com>
2026-03-17 22:42:36 -0300

    Ajuste no visual do admin e criação de tabela de marcas.

--- Django Master/carros/cars/models.py
+++ Django Master/carros/cars/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
 # Tabela que receberá os registros dos carros:
+
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True) # identificador único do registro de cada marca
+    name = models.CharField(max_length=200) # nome da marca do carro
+    
+    def __str__(self):
+        return self.name
+
 class Car(models.Model):
     id = models.AutoField(primary_key=True) # identificador único do registro de cada carro
     model = models.CharField(max_length=200) # modelo do carro
-    brand = models.CharField(max_length=200) # marca do carro
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT,related_name='car_brand') # marca do carro
     factory_year = models.IntegerField(blank=True, null=True) # ano de fabricação do carro
     model_year = models.IntegerField(blank=True, null=True) # ano do modelo do carro
     value = models.FloatField(blank=True, null=True) # valor do carro
+    
+    def __str__(self):
+        return self.model
