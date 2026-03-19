7ba780d3fb234f82a3537d2cbac0d5777db89c25
rigueti-bruno <bruno@rigueti.com>
2026-03-18 22:27:55 -0300

    habilitando o model para receber fotos

--- Django Master/carros/app/settings.py
+++ Django Master/carros/app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,3 +116,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # caminho para a pasta onde as imagens serão armazenadas
+MEDIA_URL = '/media/' # url para acessar as imagens no navegador
--- Django Master/carros/cars/models.py
+++ Django Master/carros/cars/models.py
@@ -15,7 +15,9 @@ class Car(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT,related_name='car_brand') # marca do carro
     factory_year = models.IntegerField(blank=True, null=True) # ano de fabricação do carro
     model_year = models.IntegerField(blank=True, null=True) # ano do modelo do carro
+    plate = models.CharField(max_length=10, blank=True, null=True) # placa do carro
     value = models.FloatField(blank=True, null=True) # valor do carro
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True) # foto do carro
     
     def __str__(self):
         return self.model
