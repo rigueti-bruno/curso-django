af1ac17b05cfd61bd9735ed8766dca97bbc44c8c
rigueti-bruno <bruno@rigueti.com>
2026-04-06 22:43:20 -0300

    Implementação do Model Form para simplificar a criação de forms e vinculação ao model. Cadastro de alguns dados para testar a integração dos modais.

--- Django Master/carros/cars/forms.py
+++ Django Master/carros/cars/forms.py
@@ -2,6 +2,7 @@ from django import forms # importa a biblioteca de formulários do Django para c
 from cars.models import Brand, Car # importa os dados do modelo Brand para serem utilizados no formulário como chave estrangeira
 # Importa o model Car para salvar os dados do formulário no banco de dados utilizando o método save() do formulário
 
+"""
 class CarForm(forms.Form): # define uma classe com os Campos do Formulário a ser exibido para o usuário
     model = forms.CharField(max_length=200)
     brand = forms.ModelChoiceField(Brand.objects.all()) # recebe os dados do Model Brand para serem selecionados
@@ -24,3 +25,11 @@ class CarForm(forms.Form): # define uma classe com os Campos do Formulário a se
         )
         car.save() # salva os dados do formulário no banco de dados utilizando o método save() do model Car
         return car
+"""
+# O código acima é um exemplo de como criar um formulário utilizando a classe forms.Form do Django
+
+    
+class CarModelForm(forms.ModelForm):
+    class Meta: # define a classe Meta para configurar o formulário baseado no modelo Car
+        model = Car # define que o formulário é baseado no modelo Car
+        fields = '__all__' # define que todos os campos do modelo Car serão utilizados no formulário
--- Django Master/carros/cars/views.py
+++ Django Master/carros/cars/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render, redirect #importa as funções render e redirect
 from cars.models import Car # importa os dados do modelo Car para serem utilizados na view
-from cars.forms import CarForm # importa os dados do formulário CarForm para serem utilizados na view
+from cars.forms import CarModelForm # importa os dados do formulário CarForm para serem utilizados na view
 
 # View/Função que será retornada pelo URL
 def carros(request):
@@ -21,12 +21,12 @@ def carros(request):
     
 def new_car_view(request):
     if request.method == 'POST':
-        new_car_form = CarForm(request.POST,request.FILES) # cria um formulário utilizando a classe CarForm e os dados recebidos do formulário enviado pelo usuário
+        new_car_form = CarModelForm(request.POST,request.FILES) # cria um formulário utilizando a classe CarModelForm e os dados recebidos do formulário enviado pelo usuário
         if new_car_form.is_valid(): # verifica se os dados do formulário são válidos
             new_car_form.save() # salva os dados do formulário no banco de dados
             return redirect("cars_list") # redireciona o usuário para a página de listagem de carros após o cadastro de um novo carro
     else:
-        new_car_form = CarForm() # cria um formulário vazio utilizando a classe CarForm    
+        new_car_form = CarModelForm() # cria um formulário vazio utilizando a classe CarModelForm    
     return render(
         request,
         'new_car.html', # vincula o template do formulário à view
